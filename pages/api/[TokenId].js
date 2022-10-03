08fef99eff7628a07326fdbd29784f6baaec9bbf
Kineus <108695044+Kineus@users.noreply.github.com>
2022-10-04 00:03:55 +0100

    Updated

--- pages/api/[TokenId].js
+++ pages/api/[TokenId].js
@@ -1,9 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
- const tokenId = req.query.tokenId;
- 
- const name = 'Noisy Investor #${tokenId}';
- const description = "Noisy Investor collection is an NFT Collection for all Students in the Noisy Investor Academy";
- const image = "";
+  const tokenId = req.query.tokenId;
+
+  const name = "Noisy Investor #${tokenId}";
+  const description =
+    "Noisy Investor collection is an NFT Collection for all Students in the Noisy Investor Academy";
+  const image = "";
+
+  return res.json({
+    name: name,
+    description: description,
+    image: image,
+  });
 }
